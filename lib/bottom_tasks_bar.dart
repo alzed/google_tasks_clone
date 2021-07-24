6395ea006f12e9d16ccb6b5831b232946661a46f
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-07-25 01:33:03 +0530

    Add task list bottom sheet and move bottom navigation bar out of main

---
+++ lib/bottom_tasks_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class BottomTasksBar extends StatelessWidget {
+  const BottomTasksBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            splashRadius: 20.0,
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: buildTaskListViewSheet,
+              );
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            splashRadius: 20.0,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildTaskListViewSheet(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Task List 1'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Task List 2'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Task List 3'),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'bottom_tasks_bar.dart';
+
 void main() {
   runApp(TasksApp());
 }
@@ -17,29 +19,13 @@ class TasksApp extends StatelessWidget {
   }
 }
 
-class BottomTasksBar extends StatelessWidget {
-  const BottomTasksBar({
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          IconButton(
-            icon: Icon(Icons.menu),
-            splashRadius: 20.0,
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            splashRadius: 20.0,
-            onPressed: () {},
           ),
         ],
       ),
-    );
-  }
 }
