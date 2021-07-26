238ae41b3aa67a833ce81bc20e889a0737b054ca
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-07-27 01:10:21 +0530

    Add tasks bottom sheet to bottom bar

--- lib/bottom_tasks_bar.dart
+++ lib/bottom_tasks_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'tasks_bottom_sheet.dart';
 
 class BottomTasksBar extends StatelessWidget {
   const BottomTasksBar({
@@ -39,90 +40,75 @@ class BottomTasksBar extends StatelessWidget {
   }
 
   Widget buildTaskListViewSheet(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        color: Colors.grey.shade600,
-        child: Container(
-          padding: EdgeInsets.only(
-            top: 16.0,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(4.0),
-              topLeft: Radius.circular(4.0),
+    return TasksBottomSheet(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 6.0),
+            child: TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(Colors.blueGrey.withAlpha(80)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.horizontal(
+                      right: Radius.circular(30.0),
+                    ),
+                  ),
+                ),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 35.0, top: 8.0, bottom: 8.0),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'My Tasks',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.blueAccent.shade700,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 6.0),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                        Colors.blueGrey.withAlpha(80)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.horizontal(
-                          right: Radius.circular(30.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 35.0, top: 8.0, bottom: 8.0),
-                    child: Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        'My Tasks',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.blueAccent.shade700,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Divider(
-                thickness: 2.0,
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.add,
-                        color: Colors.black87,
-                        size: 20.0,
-                      ),
-                    ),
-                    Text(
-                      'Create new list',
-                      style: TextStyle(
-                        color: Colors.black87,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+          Divider(
+            thickness: 2.0,
           ),
-        ),
+          TextButton(
+            onPressed: () {},
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.black87,
+                    size: 20.0,
+                  ),
+                ),
+                Text(
+                  'Create new list',
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 16.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
   Widget buildTaskOptionsSheet(BuildContext context) {
-    return SingleChildScrollView(
+    return TasksBottomSheet(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
